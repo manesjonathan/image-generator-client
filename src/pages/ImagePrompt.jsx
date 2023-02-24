31813c96b8da05d33bd9d60aea3e8e52392a6b01
Jonathan Manes <manesjona@gmail.com>
2023-02-24 18:43:59 +0100

    improvements and styling css

--- src/pages/ImagePrompt.jsx
+++ src/pages/ImagePrompt.jsx
@@ -1,50 +1,46 @@
 import axios from "axios";
 import {useState} from "react";
 import {URL} from "../config/config.js";
-import {CircleLoader} from "react-spinners";
+import {ClimbingBoxLoader} from "react-spinners";
 
 function ImagePrompt() {
     const [prompt, setPrompt] = useState("")
     const [url, setUrl] = useState("")
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
 
     function handleSubmit(e) {
         e.preventDefault()
-
+        setIsLoading(true);
         axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("jwt");
         axios.post(`${URL}/image/generate`, prompt).then(res => {
             setUrl(res.data);
-            setIsLoading(false)
-        })
+        }).finally(() => {
+            setIsLoading(false); // Set isLoading back to false after receiving the data
+        });
     }
 
     return (
-        <section className={'w-full min-h-screen bg-gray-800'}>
-            <article className={' items-center w-full'}>
-                <form className="p-8 m-auto items-center w-full" method="post" onSubmit={handleSubmit}>
-                    <div className={'flex'}>
-                        <label htmlFor="text"
-                               className="block w-full text-sm font-medium text-gray-900 dark:text-white">
-                            <input type="text" name="text" id="text"
-                                   onChange={e => setPrompt(e.target.value)}
-                                   className="m-auto w-full bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   placeholder="Enter something cool" required=""/></label>
-                        <button type="submit"
-                                className="block ml-4 m-auto text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-orange-500">
-                            SEND
-                        </button>
-                    </div>
-                </form>
+        <section className={'w-full min-h-screen flex flex-col justify-center items-center'}>
+            <form className="p-8 m-auto items-center flex w-2/3" method="post" onSubmit={handleSubmit}>
+                <label htmlFor="text"
+                       className="block w-full text-sm font-medium text-gray-900 dark:text-white">
+                    <input type="text" name="text" id="text"
+                           onChange={e => setPrompt(e.target.value)}
+                           className="m-auto w-full border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2.5 bg-indigo-900 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="Enter something cool" required=""/></label>
+                <button type="submit"
+                        className="block ml-4 m-auto text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-indigo-900 hover:bg-orange-500">
+                    SEND
+                </button>
+            </form>
+
+
+            <article className={'h-full w-full pb-14 flex flex-col items-center '}>
+                {isLoading && <ClimbingBoxLoader className={"m-28"} color='#fff' size={30}/>}
+
+                {url && <img className={"m-auto shadow-xl "} width={512} height={512} src={url} alt=""/>}
             </article>
 
-
-            {(url &&
-                <article className={'w-full bg-gray-800'}>
-                    {(isLoading && <CircleLoader/>)}
-
-                    <img className={"m-auto shadow-xl "} width={512} height={512} src={url} alt=""/>
-                </article>
-            )}
         </section>
     );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,6 @@ function Login() {
 
         try {
             axios.post(`${URL}/user/login`, user).then(res => {
-                console.log(res.data)
                 localStorage.setItem("jwt", res.data);
                 navigate("/dashboard");
             })
@@ -27,35 +26,30 @@ function Login() {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <section className="">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0 ">
+                <div
+                    className="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0 backdrop-filter backdrop-blur-md bg-transparent border-2 border-fuchsia-200">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center">
                             Sign in to your account
                         </h1>
-                        <form className="space-y-4 md:space-y-6" method="post" onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="email"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                    email</label>
-                                <input type="email" name="email" id="email"
-                                       onChange={e => setEmail(e.target.value)}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@company.com" required/>
-                            </div>
-                            <div>
-                                <label htmlFor="password"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
-                                       onChange={e => setPassword(e.target.value)}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required/>
-                            </div>
+                        <form className="" method="post" onSubmit={handleSubmit}>
+                            <label htmlFor="email"
+                                   className="mb-2 block text-sm font-medium text-white">YOUR EMAIL</label>
+                            <input type="email" name="email" id="email"
+                                   onChange={e => setEmail(e.target.value)}
+                                   className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-indigo-900 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   placeholder="name@company.com" required/>
+                            <label htmlFor="password"
+                                   className="mt-8 mb-2 block text-sm font-medium text-gray-900 dark:text-white">PASSWORD</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                   onChange={e => setPassword(e.target.value)}
+                                   className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-indigo-900 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                   required/>
 
                             <button type="submit"
-                                    className="w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 hover:bg-orange-500">Sign
-                                in
+                                    className="mt-8  w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800 bg-indigo-900 hover:bg-orange-500">LOGIN
                             </button>
 
                         </form>
