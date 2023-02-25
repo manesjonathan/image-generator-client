36dc3f20c6c2f2363f886822074ef53cf4407531
Jonathan Manes <manesjona@gmail.com>
2023-02-25 13:09:32 +0100

    minor change

--- src/pages/ImagePrompt.jsx
+++ src/pages/ImagePrompt.jsx
@@ -27,7 +27,7 @@ function ImagePrompt() {
                         <input type="text" name="text" id="text"
                                onChange={e => setPrompt(e.target.value)}
                                className="m-auto w-full border sm:text-sm rounded-lg block p-2.5 bg-indigo-900 border-gray-600 focus:ring-blue-500 focus:border-blue-500 placeholder:text-white"
-                               placeholder="Enter something cool" required=""/></label>
+                               placeholder="Enter something cool..." required=""/></label>
                     <button type="submit"
                             className="block ml-4 m-auto text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-indigo-900 hover:bg-orange-500">SEND
                     </button>
@@ -38,9 +38,7 @@ function ImagePrompt() {
 
                     {url && <img className={"m-auto shadow-xl "} width={512} height={512} src={url} alt=""/>}
                 </article>
-
             </main>
-
     );
 }
 
