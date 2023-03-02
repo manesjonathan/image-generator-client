a96a7554859eed25fd3a629d49bab0ff1666d227
Jonathan Manes <manesjona@gmail.com>
2023-03-02 16:30:40 +0100

    Delete if role = admin

--- src/components/ImageFullScreen.jsx
+++ src/components/ImageFullScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {FaTrash} from "react-icons/fa"
 import axios from "axios";
@@ -6,27 +6,42 @@ import {URL} from "../config/config";
 
 const ImageFullScreen = () => {
 
-    const {id} = useParams();
-    const navigate = useNavigate();
+        const {id} = useParams();
+        const navigate = useNavigate();
+        const [role, setRole] = useState(false);
 
-    const handleDelete = () => {
-        axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("jwt");
-        axios.delete(`${URL}/image/delete`, {
-            params: {name: id}
-        }).then(() => {
-            navigate("/gallery");
-        })
-        console.log('delete done')
-    };
+        useEffect(() => {
+            let item = JSON.parse(localStorage.getItem("roles"));
+            console.log(item);
+            item.forEach(role => {
+                if (role.name === "ADMIN") {
+                    setRole(true);
+                }
+            });
+        }, [role]);
+        const handleDelete = () => {
+            axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("jwt");
+            axios.delete(`${URL}/image/delete`, {
+                params: {name: id}
+            }).then(() => {
+                navigate("/gallery");
+            })
+            console.log('delete done')
+        };
 
-    return (
-        <section className={'flex flex-col items-center justify-center h-screen-custom w-full'}>
-            <div className={'items-end flex flex-col'}>
-                <img width={512} src={`https://dall-e-history.s3.eu-west-3.amazonaws.com/${id}`} alt="Full screen"/>
-                <button className={'flex mt-2 justify-center items-center text-white'} onClick={handleDelete}><p>DELETE PICTURE</p><FaTrash className={'ml-2'}/></button>
-            </div>
-        </section>
-    );
-};
+        return (
+            <section className={'flex flex-col items-center justify-center h-screen-custom w-full'}>
+                <div className={'items-end flex flex-col'}>
+                    <img width={512} src={`https://dall-e-history.s3.eu-west-3.amazonaws.com/${id}`} alt="Full screen"/>
+                    {role &&
+                        <button className={'flex mt-2 justify-center items-center text-white'} onClick={handleDelete}>
+                            <p>DELETE PICTURE</p>
+                            <FaTrash className={'ml-2'}/>
+                        </button>}
+                </div>
+            </section>
+        );
+    }
+;
 
 export default ImageFullScreen;
--- src/config/UtilService.js
+++ src/config/UtilService.js
@@ -3,7 +3,15 @@ import {URL} from "./config.js";
 
 export const loginFunction = (user, navigate) => {
     return axios.post(`${URL}/user/login`, user).then(res => {
-        localStorage.setItem("jwt", res.data);
+        console.log(res.data)
+        Object.keys(res.data).forEach(function (key) {
+            localStorage.setItem("jwt", key);
+            console.log(key)
+
+            localStorage.setItem("roles", JSON.stringify(res.data[key]));
+            console.log(res.data[key]);
+
+        });
         navigate("/generate");
     })
 }
